2a80fd76bf51d897636b8ccde7b432fbb41bddae
iosue <joshua.j.cassell@gmail.com>
2019-12-03 19:35:51 +0000

    challenge 10 updated

--- 10/main.cpp
+++ 10/main.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
+
 	int is_prime(long int num){
-		bool is_prime = true;
 		for (long int i=2;i<=num-1;i++) {
 			if (num%i==0) {
-				is_prime = false;
-				cout << num << " " << is_prime << "\n";
-				return is_prime;
+				return 0;
 			}
 		}
-		is_prime = true;
-		cout << num << " " << is_prime << "\n";
-		return is_prime;
+		return 1;
 	}
 
 	int sum_below(int below){
-		long long int sum = 0; 
+		long long int sum = 0;
 		for (int i=2;i<=below;i++) {
-			if (is_prime(i)==true) {
+			if (is_prime(i)) {
 				sum = sum + i;
+				cout << sum << " " << (i / 20000) << "%" << "\n";
+				//cout << i << "\n";
 			}
 		}
 	return sum;
 	}
 
     int main(){
+	    	cout << is_prime(6) << "\n";
 		cout << sum_below(2000000) << "\n";
     return 0;
 	}
