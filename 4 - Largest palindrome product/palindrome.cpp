9fac0d18685d42762120e578afe6875269546d9c
izzy 🌺🌺 <cassell.izzy@gmail.com>
2021-04-19 23:41:46 +0100

    cpp

---
+++ 4 - Largest palindrome product/palindrome.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+int palindrome(int x) {
+    string str = to_string(x);
+    int l = str.length();
+    for (int i = 0; i <= l; i++) {
+	if (str[i] != str[l - i - 1]) {
+	    return false;
+	}
+    }
+    return true;
+}
+
+int main() {
+    int n = 0;
+    for (int i = 100; i <= 999; i++) {
+	for (int j = 100; j <= 999; j++) {
+	    if (palindrome(i*j) && i*j > n) {
+		n = i*j;
+	    }
+	}
+    }
+    cout << n << endl;
+    //cout << palindrome(9009);
+}
+
