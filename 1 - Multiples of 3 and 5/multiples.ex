11d229afc9054beffc3726729e6ba07ca105c98d
izzuishere <cassell.izzy@gmail.com>
2021-05-22 23:46:34 +0100

    completed in elixir

--- 1 - Multiples of 3 and 5/multiples.ex
+++ 1 - Multiples of 3 and 5/multiples.ex
@@ -7,16 +7,24 @@ defmodule Multiples do
     Find the sum of all the multiples of 3 or 5 below 1000.
   """
 
+  # Solution with recursion
   def sum(x, max) when x >= max do 0 end
-
   def sum(x, max) when rem(x,3) == 0 or rem(x,5) == 0 do
     x + sum(x + 1, max)
   end
-
   def sum(x, max) do
     sum(x + 1, max)
+  end
+
+  # Solution with enums
+  def enum(x, max) do
+    Enum.sum(
+      Enum.filter(x..max-1, fn x -> rem(x,3) == 0 or rem(x,5) == 0 end)
+    )
   end
 
 end
 
 IO.puts(Multiples.sum(1, 1000))
+
+#IO.puts(Multiples.enum(1,1000))
